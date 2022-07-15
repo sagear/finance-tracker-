01477b6b6454847b7a3f866bff74890db33e0dd3
Sagar Bandari <sagearonn@gmail.com>
2022-07-15 18:35:53 +0530

    Secret keys moved in credentials

---
+++ config/initializers/iex_cloud.rb
@@ -0,0 +1,6 @@
+# Configure IEX::Api.configure do |config|
+#   config.publishable_token = 'token' # defaults to 
+#   ENV['IEX_API_PUBLISHABLE_TOKEN']
+#   config.endpoint = 'https://sandbox.iexapis.com/v1' # defaults to 
+#   'https://cloud.iexapis.com/v1'
+# end 
